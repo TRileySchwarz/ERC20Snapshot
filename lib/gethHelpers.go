4fe83e7ddcf739fd9000c2d3e285d02e48bb8114
TRileySchwarz <timothy@vanbex.com>
2019-06-07 16:27:44 -0700

    added more features

--- lib/gethHelpers.go
+++ lib/gethHelpers.go
@@ -6,36 +6,57 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"io/ioutil"
 	"log"
 	"math/big"
+	"net/http"
 )
 
-type GetLogPayload struct {
-	Jsonrpc string         `json:"jsonrpc,omitempty"`
-	Method  string         `json:"method,omitempty"`
-	Params  []GetLogParams `json:"params,omitempty"`
-	ID      int            `json:"id,omitempty"`
+//type GetLogPayload struct {
+//	Jsonrpc string         `json:"jsonrpc,omitempty"`
+//	Method  string         `json:"method,omitempty"`
+//	Params  []GetLogParams `json:"params,omitempty"`
+//	ID      int            `json:"id,omitempty"`
+//}
+//
+//type GetLogParams struct {
+//	FromBlock string `json:"fromBlock,omitempty"`
+//	ToBlock string `json:"toBlock,omitempty"`
+//	Address   string `json:"address,omitempty"`
+//}
+//
+//type GetTxPayload struct {
+//	Jsonrpc string   `json:"jsonrpc,omitempty"`
+//	Method  string   `json:"method,omitempty"`
+//	Params  []string `json:"params,omitempty"`
+//	ID      int      `json:"id,omitempty"`
+//}
+//
+//type GetBlockByNumberPayload struct {
+//	Jsonrpc string        `json:"jsonrpc,omitempty"`
+//	Method  string        `json:"method,omitempty"`
+//	Params  []interface{} `json:"params,omitempty"`
+//	ID      int           `json:"id,omitempty"`
+//}
+
+type Snapshot struct {
+	TokenAddress string `json:"tokenAddress,omitempty"`
+	StartBlock string `json:"startBlock,omitempty"`
+	EndBlock string `json:"endBlock,omitempty"`
+	Balances []WalletAddress `json:"balances,omitempty"`
 }
 
-type GetLogParams struct {
-	FromBlock string `json:"fromBlock,omitempty"`
-	ToBlock string `json:"toBlock,omitempty"`
-	Address   string `json:"address,omitempty"`
+type WalletAddress struct {
+	Address string `json:"address,omitempty"`
+	WalletDetails Wallet `json:"walletDetails,omitempty"`
 }
 
-type GetTxPayload struct {
-	Jsonrpc string   `json:"jsonrpc,omitempty"`
-	Method  string   `json:"method,omitempty"`
-	Params  []string `json:"params,omitempty"`
-	ID      int      `json:"id,omitempty"`
+type Wallet struct {
+	Balance string `json:"balance,omitempty"`
 }
 
-type GetBlockByNumberPayload struct {
-	Jsonrpc string        `json:"jsonrpc,omitempty"`
-	Method  string        `json:"method,omitempty"`
-	Params  []interface{} `json:"params,omitempty"`
-	ID      int           `json:"id,omitempty"`
-}
+var EtherscanBaseURl = "https://api.etherscan.io/api?module=account&action=tokentx"
+
 
 func BuildSnapshot(tokenAddress string, provider string, block int64) {
 
@@ -44,13 +65,15 @@ func BuildSnapshot(tokenAddress string, provider string, block int64) {
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
+
 	// Instantiate the contract and display its name
 	token, err := NewERC20Token(common.HexToAddress(tokenAddress), conn)
 	if err != nil {
 		log.Fatalf("Failed to instantiate a Token contract: %v", err)
 	}
 
-	arrayOfWallets := GetTokenWallets()
+	// Returns all wallets located in the token
+	arrayOfWallets := GetTokenWallets(tokenAddress)
 
 	for _, element := range arrayOfWallets{
 		GetBalanceAtBlock(element, block, token)
@@ -71,7 +94,46 @@ func GetBalanceAtBlock(walletAddress string, block int64, token *ERC20Token) {
 	fmt.Printf("Token balance for address %v - %v \n", walletAddress, balance)
 }
 
-func GetTokenWallets() ([]string) {
+// We will use Etherescan to build a list of all wallets holding tokens at a given block.
+// We can verify these numbers as totalling all the holders should equal the total supply.
+// By using two different sources, Etherscan and the chosen Geth Node, you can ensure your data is credible
+// Versus relying on one source for both pieces of information
+func GetTokenWallets(tokenAddress string) ([]string) {
+
+	url := EtherscanBaseURl + "&contractaddress=" + tokenAddress
+	// + "&page=" 1
+	// + "&offset=" 100
+	// + "&sort=" asc
+	//+ "&apikey=" YourApiKeyToken
+
+	println(url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+
+
+	test := string(body)
+
+	var response GetLogsResponse
+
+	json.Unmarshal([]byte(test), &response)
+
+	bs, _ := json.Marshal(response)
+
+	fmt.Printf("%s", bs)
+
+
 	arrayToReturn := []string{"0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE", "0x0D0707963952f2fBA59dD06f2b425ace40b492Fe"}
 
 	return arrayToReturn
@@ -163,19 +225,19 @@ func GetTokenWallets() ([]string) {
 //	printGetTx(readerBody)
 //}
 
-func printGetLogs(bytesString string) {
-	var response GetLogsResponse
-
-	json.Unmarshal([]byte(bytesString), &response)
-	bs, _ := json.Marshal(response)
-
-	if(response.Error.Message != "") {
-		fmt.Println("\n Oops, there was an error in the request!")
-		fmt.Printf("%s", bs)
-	} else {
-		fmt.Printf("%s", bs)
-	}
-}
+//func printGetLogs(bytesString string) {
+//	var response GetLogsResponse
+//
+//	json.Unmarshal([]byte(bytesString), &response)
+//	bs, _ := json.Marshal(response)
+//
+//	if(response.Error.Message != "") {
+//		fmt.Println("\n Oops, there was an error in the request!")
+//		fmt.Printf("%s", bs)
+//	} else {
+//		fmt.Printf("%s", bs)
+//	}
+//}
 
 //func printGetTx(bytesString string) {
 //	var response GetTxResponse
